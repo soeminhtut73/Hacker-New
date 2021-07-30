c4b2bf367b0763dbe8d15b060bab2bdffd40c298
smh <soeminhtut73@gmail.com>
2021-07-30 13:19:26 +0630

    Initial Commit

---
+++ Hacker New/Hacker_NewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewApp.swift
+//  Hacker New
+//
+//  Created by MAC on 30/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
